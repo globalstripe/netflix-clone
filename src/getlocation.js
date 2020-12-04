83f8862f0c7f0bbeae8eb2aadcad1591e4d76b0c
Chris Clark <chris.clark@globalstripe.com>
2020-12-04 22:48:32 +0000

    BackandReload

--- src/getlocation.js
+++ src/getlocation.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import MapContainer from './googlemaps.js'
+import { Link } from 'react-router-dom';
 
 import './geolocation.css'
 
@@ -45,6 +46,7 @@ function BrowserLocation() {
 
     return (
       <div className="profile">
+        <Link to="/"><h1>Back</h1></Link><a href='/BrowserLocation'><h1>Reload</h1></a>
         <h1>Using geolocation JavaScript API in React</h1>
         <h1><p>Lattitude:   {latitude} </p> </h1>
         <h1><p>Longtitude:   {longtitude} </p></h1>
